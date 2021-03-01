ef3c4705b47fdaadf98d3b54bde4f6332b226a7d
zhuoya <2327524047@qq.com>
2021-03-01 11:27:20 +0800

    1 commit supermall

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+   <div id="app"></div>
+</template>
+
+<script>
+export default {
+   data () {
+      return {
+
+      };
+   },
+
+   components: {},
+
+   computed: {},
+
+   methods: {}
+}
+</script>
+<style lang='css' scoped>
+
+</style>
